2416662eee73e627bb0b3275479d1cf036b421f3
Ahmet Sancar <tpasancar@architecht.com>
2021-11-08 12:21:28 +0300

    Initial Commit

---
+++ swiftuitest/swiftuitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuitestApp.swift
+//  swiftuitest
+//
+//  Created by Ahmet Sancar on 8.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftuitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
